2ca7f1dd94fefb877775641503238e8aa6edf89e
temsan89 <temsan89@gmail.com>
2017-11-24 12:52:16 +0200

    before generic view

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,7 @@
+import datetime
 from django.db import models
-from django.utils import timesince
+from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
-
 
 @python_2_unicode_compatible
 class Question(models.Model):
